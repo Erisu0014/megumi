7d2d9e91e3521fd2fbbc843b9957478ded65637d
alice <1269732086@qq.com>
2021-06-17 14:39:34 +0800

    2021/6/17 sqlite->mysql

--- megumi/src/main/java/com/erisu/cloud/megumi/MegumiApplication.java
+++ megumi/src/main/java/com/erisu/cloud/megumi/MegumiApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableScheduling
+@PropertySource("classpath:mysql.properties")
 @PropertySource("classpath:redis.properties")
 @PropertySource("classpath:qq.properties")
 @EnableAsync
