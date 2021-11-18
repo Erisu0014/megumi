a94a2550be5e49736f3f91550704abf4be22735e
Erisu <1269732086@qq.com>
2021-11-18 17:44:42 +0800

    :memo: 更新jp.json

--- megumi/src/main/java/com/erisu/cloud/megumi/remind/pojo/RemindType.kt
+++ megumi/src/main/java/com/erisu/cloud/megumi/remind/pojo/RemindType.kt
@@ -16,5 +16,5 @@ import lombok.Getter
 enum class RemindType(var activity: String, var remindBegin: Int, var remindLast: Int) {
     gvg("公会战", 1, 1), fes("剧情活动", 1, 1), gacha("卡池", 0, 1),
     playoff("季前赛", 1, 0), normal("normal", 1, 0), hard("hard", 1, 0),
-    temple("神殿", 0, 0), princess_chip("心碎", 0, 0)
+    temple("神殿", 0, 0), princess_chip("心碎", 0, 0), tower("露娜塔", 0, 1)
 }
