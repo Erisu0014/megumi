8f0e48376440f5ae83830faadc2ad2b16e5c3c1c
Erisu <1269732086@qq.com>
2022-05-05 15:23:37 +0800

    :art: 优化asoul查重判断

--- megumi/src/main/java/com/erisu/cloud/megumi/asoul/service/AsoulService.kt
+++ megumi/src/main/java/com/erisu/cloud/megumi/asoul/service/AsoulService.kt
@@ -5,10 +5,13 @@ import com.erisu.cloud.megumi.command.Command
 import com.erisu.cloud.megumi.command.CommandType
 import com.erisu.cloud.megumi.pattern.Pattern
 import com.erisu.cloud.megumi.plugin.pojo.Model
+import io.ktor.network.sockets.*
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.message.data.Message
 import net.mamoe.mirai.message.data.MessageChain
+import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.content
 import org.springframework.stereotype.Component
 import javax.annotation.Resource
 
@@ -31,9 +34,10 @@ class AsoulService {
 
     @Command(value = "",
         commandType = CommandType.GROUP,
-        pattern = Pattern.PREFIX,
+        pattern = Pattern.CHECK,
         uuid = "0bc353b425a1409bb4c9a473d8bab737")
     fun doubleCheckDefault(sender: User?, messageChain: MessageChain, subject: Contact?): Message? {
+        // 过滤qq小程序
         val content = messageChain.contentToString()
         if (content.length > 400) {
             return asoulLogic.doubleCheck(content.trim())
